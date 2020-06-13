9b1c8d6c46a25c3efa9bf6e02263a930f0b12cbd
darkspook <jcba003@gmail.com>
2020-06-13 02:51:23 +0000

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks line NO LOCAL FILE. You must be on PRODUCTION.")
